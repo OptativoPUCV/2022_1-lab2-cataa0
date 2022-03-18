897eda2e9de1f8415f0c380adb344eda91b0bf74
cataa0 <>
2022-03-18 14:50:53 +0000

    new try

--- list.c
+++ list.c
@@ -46,7 +46,7 @@ void * firstList(List * list) {
 
 void * nextList(List * list) {
   if(list->current && list->current->next){
-    list->cuurent =list->current->next;
+    list->current =list->current->next;
     return list->current->data;
   }
     return NULL;
