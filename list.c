17084d6142bfc522b436b79c82722fbd1de21fe2
cataa0 <>
2022-03-18 14:29:30 +0000

    new try

--- list.c
+++ list.c
@@ -37,6 +37,10 @@ List * createList() {
 }
 
 void * firstList(List * list) {
+  if(list->head && list->head->data){
+    list->current=list->head;
+    return list->head->data;
+  }
     return NULL;
 }
 
