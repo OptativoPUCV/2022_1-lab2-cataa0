063d7c3adeee073dc4c2b64f67146a76a02f1024
cataa0 <>
2022-03-18 14:54:09 +0000

    new try

--- list.c
+++ list.c
@@ -61,7 +61,11 @@ void * lastList(List * list) {
 }
 
 void * prevList(List * list) {
-  
+  if(list->current && list->current->prev){
+    list->current = list->current->prev;
+    return list->current->data;
+    
+  }
     return NULL;
 }
 
