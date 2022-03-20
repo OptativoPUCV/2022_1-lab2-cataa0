08defd02f64c0dc9cbdeab88a5f01c3bf3df3999
cataa0 <>
2022-03-20 14:37:51 +0000

    new try

--- list.c
+++ list.c
@@ -88,7 +88,7 @@ void pushBack(List * list, void * data) {
 
 void pushCurrent(List * list, void * data) {
   Node *node = createNode(data);
-  if(list->current->next != NULL){ 
+  if(list->current->next == NULL){ 
     list->current->next->prev=node;
     list->current->next=node->next;
     list->current->next=node;
