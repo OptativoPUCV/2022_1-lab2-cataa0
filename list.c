7c31ee85d8a501fd780e6e245276f0398a465abb
cataa0 <>
2022-03-20 20:51:40 +0000

    new try

--- list.c
+++ list.c
@@ -121,7 +121,7 @@ void * popCurrent(List * list) {
     list->head=list->current->next;
     list->current->next->prev=NULL;
     list->current=list->current->next;
-    list->head=list->current;
+    list->current=list->head;
 
   }
   if(list->current == list->tail){
